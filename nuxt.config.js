d914fd4f945363763fdbded1b8a6508d074b8a30
Bugado <addminplayerbr1122@gmail.com>
2021-12-22 11:06:44 -0300

    fix: Font

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,11 +12,14 @@ export default {
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{ hid: 'description', name: 'description', content: '' }
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap' }
+		]
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: ['@/assets/global', '@/static/font'],
+	css: ['@/assets/global'],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: ['@/plugins/accessor', '@/plugins/axios'],
